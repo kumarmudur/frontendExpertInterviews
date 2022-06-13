447373e40f2d4f318f046599c50cc4dfee2a3091
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-06-13 14:34:46 +0530

    [Javascript] debounce examples

---
+++ javascript/debounce.js
@@ -0,0 +1,21 @@
+const debounce = (callback, delay, immediate = false) => {
+    let timerID;
+    return function (...args) {
+      clearTimeout(timerID);
+  
+      const shouldCallImmediately = timerID == null && immediate;
+      if (shouldCallImmediately) {
+        callback.apply(this, args);
+      }
+  
+      timerID = setTimeout(() => {
+        if (!immediate) {
+          callback.apply(this, args);
+        }
+        timerID = null;
+      }, delay);
+    };
+}
+  
+exports.debounce = debounce;
+  
