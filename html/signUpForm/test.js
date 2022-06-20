72b4ecb564b3f4f1efb376758f1c0a1c15c269ec
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-06-20 06:21:26 +0530

    [HRML] sign up form test cases

--- html/signUpForm/test.js
+++ html/signUpForm/test.js
@@ -4,7 +4,7 @@ const { JSDOM } = require('jsdom');
 const { readFileSync } = require('fs');
 const { expect } = require('chai');
 
-const hmtl = readFileSync('./sign_up_form.html');
+const html = readFileSync('./sign_up_form.html');
 
 let inputs;
 let labels;
@@ -20,4 +20,35 @@ const resetDom = () => {
 
     inputs = document.querySelectorAll('input');
     labels = document.querySelectorAll('label');
-}
+};
+
+const EXPECTED_LABELS = [
+  'Username:',
+  'Password:',
+  'Email:',
+  'Phone Number:',
+  'Date of Birth:',
+  'I agree to the TOS',
+];
+
+describe('sign-up form', () => {
+    beforeEach(() => {
+        resetDom();
+    });
+
+    describe('labels', () => {
+        it('have the correct text content', () => {
+            labels.forEach((label, i) => {
+                expect(label.textContent.trim()).to.equal(EXPECTED_LABELS[i]);
+            });
+        });
+    });
+
+    describe('inputs', () => {
+        it('username field has the correct attributes', () => {
+            const input = inputs[0];
+            expect(input.type).to.equal('text');
+            expect(input.hasAttribute('require')).to.be.true;
+        });
+    });
+});
