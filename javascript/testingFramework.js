8cc717c57d667296b566586d317b224bf019fc69
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-06-29 06:49:53 +0530

    [JS] testing framework examples

---
+++ javascript/testingFramework.js
@@ -0,0 +1,64 @@
+function describe(testSuiteName, func) {
+    console.log(`beginning test suite ${testSuiteName}`);
+  
+    try {
+      func();
+      console.log(`successfully completed test suite ${testSuiteName}`);
+    } catch (error) {
+      const { testCaseName, errorMessage } = error;
+      console.error(
+        `failed running test suite ${testSuiteName} on ` +
+        `test case ${testCaseName} with error message ${errorMessage}`,
+      );
+    }
+}
+  
+function it(testCaseName, func) {
+    console.log(`beginning test case ${testCaseName}`);
+  
+    try {
+      func();
+      console.log(`successfully completed test case ${testCaseName}`);
+    } catch (errorMessage) {
+      throw { testCaseName, errorMessage};
+    }
+}
+  
+function expect(actual) {
+    return new ExpectFunctions(actual);
+}
+  
+class ExpectFunctions {
+    constructor(actual) {
+      this.actual = actual;
+      this.stringifieldActual = JSON.stringify(actual);
+    }
+  
+    toExist() {
+      if (this.actual == null) {
+        throw `expected value to exist but got ${this.stringifieldActual}`;
+      }
+    }
+  
+    toBe(expected) {
+      if (this.actual !== expected) {
+        throw `expected ${this.stringifieldActual} to be ${JSON.stringify(
+          expected,
+        )}`;
+      }
+    }
+  
+    toBeType(type) {
+      if (typeof this.actual !== type) {
+        throw `expected ${
+          this.stringifieldActual
+        } to be of type ${type} but got ${typeof this.actual}`;
+      }
+    }
+}
+  
+// Do not edit the lines below.
+exports.describe = describe;
+exports.it = it;
+exports.expect = expect;
+  
