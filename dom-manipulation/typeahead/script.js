81c2dd3a7ad30258afb75ad99a64f29ce0a946dc
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-07-28 22:03:41 +0530

    [DOM Manipulation] typeahead example js solution 2

--- dom-manipulation/typeahead/script.js
+++ dom-manipulation/typeahead/script.js
@@ -47,4 +47,53 @@ function clearSuggestions() {
   suggestionsList.innerHTML = '';
 }
 
+// Solution 2 =============================================
+
+const BASE_URL = 'https://www.algoexpert.io/api/fe/glossary-suggestions';
+
+let timeoutID;
+
+const input = document.getElementById('typeahead');
+const suggestionsList = document.getElementById('suggestions-list');
+
+input.addEventListener('input', onType);
+
+function onType() {
+  if (input.value.length === 0) {
+    clearSuggestions();
+    return;
+  }
+  clearTimeout(timeoutID);
+  timeoutID = setTimeout(fetchAndAppendSuggestions, 500);
+}
+
+async function fetchAndAppendSuggestions() {
+  const url = new URL(BASE_URL);
+  url.searchParams.set('text', input.value);
+  const response = await fetch(url);
+  const suggestions = await response.json();
+
+  const fragment = document.createDocumentFragment();
+  suggestions.forEach(suggestion => {
+    fragment.appendChild(createSuggestionElement(suggestion));
+  });
+  suggestionsList.replaceChildren(fragment);
+}
+
+function createSuggestionElement(suggestion) {
+  const suggestionElement = document.createElement('li');
+  suggestionElement.textContent = suggestion;
+  suggestionElement.addEventListener('click', () => {
+    input.value = suggestion;
+    clearSuggestions();
+  });
+  return suggestionElement;
+}
+
+function clearSuggestions() {
+  clearTimeout(timeoutID);
+  suggestionsList.innerHTML = '';
+}
+
+
 
