d654a2d78b2819b604295454d37d252f2b87a028
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-08-06 23:02:43 +0530

    [Javascript] memoized javascript example

---
+++ javascript/memoize.js
@@ -0,0 +1,39 @@
+function memoize(callback, resolver) {
+    const cache = new Map();
+  
+    function getCacheKey(args) {
+      return resolver != null ? resolver(...args) : JSON.stringify(args);
+    }
+
+    const memoized = function(...args) {
+      const cacheKey = getCacheKey(args);
+  
+      if (cache.has(cacheKey)) {
+        return cache.get(cacheKey);
+      }
+  
+    const output = callback(...args);
+      cache.set(cacheKey, output);
+      return output;
+    };
+  
+    memoized.clear = function() {
+      cache.clear();
+    };
+  
+    memoized.delete = function (...args) {
+      const cacheKey = getCacheKey(args);
+      cache.delete(cacheKey);
+    };
+  
+    memoized.has = function (...args) {
+      const cacheKey = getCacheKey(args);
+      return cache.has(cacheKey);
+    }
+  
+    return memoized;
+  }
+  
+  // Do not edit the line below.
+  exports.memoize = memoize;
+  
