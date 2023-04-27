f8012ec94de5bd43bcc0e33e077a0eef6fb948a4
kumarmudur <kumarmudur10@gmail.com>
2023-04-27 19:18:26 +0530

    [React-Components] question list component changes

---
+++ react-components/questionList/QuestionList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const QUESTIONS_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/questions';
+const SUBMISSIONS_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/submissions';
+
+export default function QuestionList() {
+  const [questions, submissions] = useQuestionsAndSubmissions();
+  const questionsByCategory = getQuestionsByCategory(questions);
+  const submissionsByQuestion = getSubmissionsByQuestion(submissions);
+  const categories = Object.keys(questionsByCategory;)
+
+  return (
+    <>
+      {
+        categories.map(category => (
+            <Category 
+                key={category}
+                category={category}
+                questions={questionsByCategory[category]}
+                submissionsByQuestion={submissionsByQuestion}
+            />
+        )
+        )
+      }
+    </>
+  );
+}
